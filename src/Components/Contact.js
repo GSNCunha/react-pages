a3eed66e87097fd9ea7ef0c152d9a3050fbbac5d
GSNCunha <61071956+GSNCunha@users.noreply.github.com>
2022-02-25 15:58:24 -0500

    email working

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,106 +1,123 @@
 import React, { Component } from 'react';
+import emailjs from "emailjs-com";
+
+
+
 
 class Contact extends Component {
-  render() {
+   render() {
 
-    return (
-      <section id="contact">
+      function sendEmail(e) {
+         e.preventDefault();
+ 
+     emailjs.sendForm('gmail', 'template_bc48enl', e.target, 'user_GTlxptiKDu3LQHwm9LrrO')
+         .then((result) => {
+             console.log(result.text);
+         }, (error) => {
+             console.log(error.text);
+         });
+         e.target.reset()
+     }
 
-         <div className="row section-head">
+      return (
+         <section id="contact">
 
-            <div className="two columns header-col">
+            <div className="row section-head">
 
-               <h1><span>Get In Touch.</span></h1>
+               <div className="two columns header-col">
 
-            </div>
+                  <h1><span>Get In Touch.</span></h1>
 
-            <div className="ten columns">
+               </div>
+
+               <div className="ten columns">
 
                   <p className="lead">Here is where you should write your message to readers to have them get in contact with you.</p>
 
+               </div>
+
             </div>
 
-         </div>
+            <div className="row">
+               <div className="eight columns">
 
-         <div className="row">
-            <div className="eight columns">
+               <form onSubmit={sendEmail}>
+                  <fieldset>
+                     <div className="row pt-5 mx-auto">
+                        <div>
+                           <label htmlFor="contactName">Name <span className="required">*</span></label>
+                           <input type="text" defaultValue="" size="35" id="contactName" placeholder="Name" name="name" onChange={this.handleChange}/>
+                        </div>
 
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
+                        <div>
+                           <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+                           <input type="email" defaultValue="" size="35" id="contactEmail" placeholder="Email Address" name="email" onChange={this.handleChange}/>
+                        </div>
 
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
+                        <div>
+                           <label htmlFor="contactSubject">Subject</label>
+                           <input type="text" defaultValue="" size="35" id="contactSubject" placeholder="Subject" name="subject" onChange={this.handleChange}/>
+                        </div>
 
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
+                        <div>
+                           <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                           <textarea cols="50" rows="15" id="contactMessage" placeholder="Your message" name="message"></textarea>
+                        </div>
 
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
+                        <div>
+                           <button className="submit">Submit</button>
+                           <span id="image-loader">
+                              <img alt="" src="images/loader.gif" />
+                           </span>
+                        </div>
+                     </div>
+                  </fieldset>
 				   </form>
 
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
+                  <div id="message-warning"> Error </div>
+                  <div id="message-success">
+                     <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+                  </div>
+               </div>
 
 
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
+               <aside className="four columns footer-widgets">
+                  <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   Gabriel Sadigursky<br />
-						   North Bay Village, Florida<br />
-						   <span>55 51 99760-0101</span>
-					   </p>
-				   </div>
+                     <h4>Address and Phone</h4>
+                     <p className="address">
+                        Gabriel Sadigursky<br />
+                        North Bay Village, Florida<br />
+                        <span>55 51 99760-0101</span>
+                     </p>
+                  </div>
 
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Post's</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://post</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://otherPost</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
-            </aside>
-      </div>
-   </section>
-    );
-  }
+                  <div className="widget widget_tweets">
+                     <h4 className="widget-title">Post's</h4>
+                     <ul id="twitter">
+                        <li>
+                           <span>
+                              This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+                              Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+                              <a href="#">http://post</a>
+                           </span>
+                           <b><a href="#">2 Days Ago</a></b>
+                        </li>
+                        <li>
+                           <span>
+                              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                              eaque ipsa quae ab illo inventore veritatis et quasi
+                              <a href="#">http://otherPost</a>
+                           </span>
+                           <b><a href="#">3 Days Ago</a></b>
+                        </li>
+                     </ul>
+                  </div>
+               </aside>
+            </div>
+         </section>
+      );
+   }
 }
 
 export default Contact;
