3376543903ebb73de5be03076c1c00b1f373cb43
GSNCunha <61071956+GSNCunha@users.noreply.github.com>
2022-02-24 20:47:39 -0500

    download resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,18 +3,6 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
-    }
 
     return (
       <section id="about">
@@ -40,7 +28,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href="http://gabrieldev.com"className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
