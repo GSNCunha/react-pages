760e95ff35039d12012e31cf16c5010bad87d279
GSNCunha <61071956+GSNCunha@users.noreply.github.com>
2022-02-27 09:41:08 -0500

    Updates

---
+++ precache-manifest.9844d3613a160ca4ecbead16d5d0f631.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "87276db66acd420b7a87",
+    "url": "/react-pages/static/js/main.87276db6.chunk.js"
+  },
+  {
+    "revision": "d5f659191ad8c77ac431",
+    "url": "/react-pages/static/js/1.d5f65919.chunk.js"
+  },
+  {
+    "revision": "c9847d069c9cae9de2c6",
+    "url": "/react-pages/static/js/runtime~main.c9847d06.js"
+  },
+  {
+    "revision": "b8d731d7472bc2849f768f8ea5f6af29",
+    "url": "/react-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-pages/precache-manifest.01a9028e2692651a0117afdc9f2841ae.js"
+  "/react-pages/precache-manifest.9844d3613a160ca4ecbead16d5d0f631.js"
 );
 
 workbox.clientsClaim();
